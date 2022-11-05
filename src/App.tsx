91e31c9a264d08b07a8a817806f2e0d9cf73db7d
Sergio Cano <canosergio90@gmail.com>
2022-11-05 14:30:10 +0100

    initial commit
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </header>
+      <body>
+        <h1>Hello</h1>
+      </body>
+    </div>
+  )
+}
+
+export default App
