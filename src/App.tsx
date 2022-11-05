2f425f70b0cde6dcc7ad339e1a0c413713430c43
Sergio Cano <canosergio90@gmail.com>
2022-11-05 15:02:17 +0100

    adding layout
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container, Grid } from '@mui/material'
 import React from 'react'
 import './App.css'
 import { TourCard } from './components/TourCard'
@@ -5,15 +6,16 @@ import { TourCard } from './components/TourCard'
 function App() {
   return (
     <div className="App">
-      <header>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </header>
-      <body>
-        <TourCard />
-      </body>
+      <Container>
+        <Grid container spacing={5}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
     </div>
   )
 }
--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -1,10 +1,12 @@
-import { Paper } from '@mui/material'
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
 
 export const TourCard = () => {
   return (
-    <Paper elevation={3} square>
-      TourCard
-    </Paper>
+    <Grid item xs={3}>
+      <Paper elevation={3} square>
+        TourCard
+      </Paper>
+    </Grid>
   )
 }
